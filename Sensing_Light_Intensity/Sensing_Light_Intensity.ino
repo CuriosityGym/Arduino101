dd5f6eb1f257923762ef8c123dfe2daf4e9a67da
Rupin <rupin.chheda@gmail.com>
2015-07-07 17:21:42 +0530

    New Excercises added

---
+++ Sensing_Light_Intensity/Sensing_Light_Intensity.ino
@@ -0,0 +1,16 @@
+int LDRPin=A2;//Setup LDR on A2 Pin
+void setup()
+{
+  Serial.begin(9600); //Setup Serial Terminal on 9600bps
+  
+}
+
+
+void loop()
+{
+  Serial.print("Light Intensity: ");//Print a string without newline
+  
+  int lightIntensity=analogRead(LDRPin);// Measure Light Intensity
+  Serial.println(lightIntensity);//Print light intensity to Serial Terminal
+  
+}
---
+++ Sensing_Light_Intensity_Variation/Sensing_Light_Intensity_Variation.ino
@@ -0,0 +1,32 @@
+int LDRPin=A2;//Setup LDR on A2 Pin
+int LEDPin=2; // Set LEd on Pin 2
+//These values below need to be changed based on ambient conditions
+int highThreshold=580;
+int lowThreshold=530;
+void setup()
+{
+  Serial.begin(9600); //Setup Serial Terminal on 9600bps
+  pinMode(LEDPin, OUTPUT);
+}
+
+
+void loop()
+{
+  Serial.print("Light Intensity: ");//Print a string without newline
+  
+  int lightIntensity=analogRead(LDRPin);// Measure Light Intensity
+  Serial.println(lightIntensity);//Print light intensity to Serial Terminal
+  if(lightIntensity> highThreshold) //LDR is covered, switch LED OFF
+  {
+      digitalWrite(LEDPin, HIGH);
+  }
+  
+  if(lightIntensity< lowThreshold) //LDR is Lit, switch LED On
+  {
+      digitalWrite(LEDPin, LOW);
+  }
+  
+  
+  
+  
+}
